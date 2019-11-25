8d3024a668e09777fc9b5ddc51de30e57ca89e6c
SterbenXIII <36611723+SterbenXIII@users.noreply.github.com>
2019-11-25 20:23:07 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -94,9 +94,9 @@ try {
         $dog = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg"
     	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
 
-    	 $response = $client->sendImg([
+    	 $response = $client->sendMessage([
         	'chat_id' => $update->message->chat->id,
-         	'photo' => $dog
+         	'text' => $dog
      	 ]);
     }
     else if($update->message->text == '/latest')
