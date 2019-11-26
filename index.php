208677839f9753c0ff70861368d4c4ef6ae85e94
SterbenXIII <36611723+SterbenXIII@users.noreply.github.com>
2019-11-26 02:41:08 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -20,11 +20,12 @@ require 'vendor/autoload.php';
 $client = new Zelenin\Telegram\Bot\Api('999479664:AAH7iB8moiLGcwoZIzEmMVkyRdObUuH5SaY'); // Set your access token
 $url = 'https://mydewidiot-bot.herokuapp.com/'; // URL RSS feed
 $update = json_decode(file_get_contents('php://input'));
-$json = file_get_contents('https://dog.ceo/api/breeds/image/random');
-$array = json_decode($json, TRUE);
+
 
 function getImg()
 {
+    $json = file_get_contents('https://dog.ceo/api/breeds/image/random');
+    $array = json_decode($json, TRUE);
     echo($array['message']);
 }
 
